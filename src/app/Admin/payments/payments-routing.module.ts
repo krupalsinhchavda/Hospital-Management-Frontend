2cc554760f006e301c4fdb8b98b7243cbfc31b29
Krupalsinh <krupal.c@novuscode.com>
2024-10-24 17:25:38 +0530

    payment updates

--- src/app/Admin/payments/payments-routing.module.ts
+++ src/app/Admin/payments/payments-routing.module.ts
@@ -1,6 +1,9 @@
 import { Routes } from "@angular/router";
 import { PaymentsComponent } from "./payments.component";
+import { AddEditPaymentComponent } from "./add-edit-payment/add-edit-payment.component";
 
 export const PaymentsRoutingModule: Routes = [
   { path: '', component: PaymentsComponent, pathMatch: 'full' },
+  { path: 'add-payment', component: AddEditPaymentComponent, pathMatch: 'full' },
+  { path: 'edit-payment/:id', component: AddEditPaymentComponent, pathMatch: 'full' },
 ];
--- src/app/Admin/payments/payments.component.ts
+++ src/app/Admin/payments/payments.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { AddEditPaymentDialogComponent } from './add-edit-payment-dialog/add-edit-payment-dialog.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './payments.component.html',
   styleUrl: './payments.component.css'
 })
@@ -17,29 +16,9 @@ export class PaymentsComponent implements OnInit {
     { patientName: 'Sam Roe', doctorName: 'Dr. Blake', caseNo: 'C125', date: '2024-10-22', amount: 3000, details: 'Checkup', status: 'Failed' }
   ];
 
-  constructor(public dialog: MatDialog) { }
+  constructor() { }
 
   ngOnInit(): void { }
-
-  openAddPaymentDialog(): void {
-    const dialogRef = this.dialog.open(AddEditPaymentDialogComponent, {
-      width: '400px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.payments.push({
-          patientName: result.patientName,
-          doctorName: result.doctorName,
-          caseNo: result.caseNo,
-          date: new Date().toISOString().split('T')[0], // set current date
-          amount: result.amount,
-          details: 'N/A', // Add any default details here
-          status: 'Pending' // Set the default status
-        });
-      }
-    });
-  }
 
   editPayment(payment: any): void {
     console.log('Edit payment:', payment);
